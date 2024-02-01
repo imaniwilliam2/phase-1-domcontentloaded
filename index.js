7c3e6bb8213fce370413f60045907eafbfa64395
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-02-01 18:24:30 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const textElement = document.getElementById('text')
+    return textElement.textContent = "This is really cool!"
+})
